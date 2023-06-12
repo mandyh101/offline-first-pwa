1958f491714342b901a918abd3faf5dfcc936017
mandy <mandy@devart.nz>
2023-06-13 10:40:27 +1200

    clear our default styles and set up layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="flex w-screen h-screen text-gray-700">
+  <aside class="flex flex-col w-64 border-r border-gray-300 bg-gray-100">
+    <!-- sidebar -->
+<!-- <TestComponent /> -->
+<h1>side bar</h1>
+  </aside>
+  <main class="flex flex-col flex-grow">
+<h1>main stuff</h1>
+  </main>
+</div>
+
+</template>
+
+<script setup>
+</script>
+
